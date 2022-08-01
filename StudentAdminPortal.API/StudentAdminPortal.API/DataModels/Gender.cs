b916f94845f65a759a389a18927a5abf266d39dc
fernandomn <fernando_m_n@hotmail.com>
2022-08-01 15:21:54 -0300

    End of Section 2 - API

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Gender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DataModels
+{
+    public class Gender
+    {
+        public Guid ID { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DomainModels/Gender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DomainModels
+{
+    public class Gender
+    {
+        public Guid ID { get; set; }
+        public string Description { get; set; }
+    }
+}
