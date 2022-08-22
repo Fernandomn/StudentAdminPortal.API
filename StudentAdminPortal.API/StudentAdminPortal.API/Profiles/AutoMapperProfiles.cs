a3d0c7cd67403afe705a9f9074be21781ac253fc
fernandomn <fernando_m_n@hotmail.com>
2022-08-22 17:22:42 -0300

    End of Section 7. Enabling APi to create new students

--- StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -14,8 +14,9 @@ namespace StudentAdminPortal.API.Profiles
 
             CreateMap<DataModels.Address, Address>().ReverseMap();
 
-            CreateMap<UpdateStudentRequest, DataModels.Student>()
-                .AfterMap<UpdateStudentRequestAfterMap>();
+            CreateMap<UpdateStudentRequest, DataModels.Student>().AfterMap<UpdateStudentRequestAfterMap>();
+
+            CreateMap<AddStudentRequest, DataModels.Student>().AfterMap<AddStudentRequestAfterMap>();
         }
     }
 }
